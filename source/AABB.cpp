5d525886ef711f14963a05df4b71415e6e66bec3
David <david.castro@enti.cat>
2022-12-14 18:38:48 +0100

    Collisions done

--- source/AABB.cpp
+++ source/AABB.cpp
@@ -6,13 +6,51 @@ AABB::AABB(Vector2 topLeft, Vector2 size)
 	this->size = size;
 }
 
-bool AABB::CheckOverlappingAABB(const AABB*)
+bool AABB::CheckOverlappingAABB(AABB* b)
 {
-	//TODO CheckOverlappingAABB
-	return false;
+    Vector2 leftUpA, leftUpB;
+    Vector2 leftDownA, leftDownB;
+    Vector2 rightUpA, rightUpB;
+    Vector2 rightDownA, rightDownB;
+
+    //Calculate the sides of rect A
+    leftUpA = this->topLeft;
+    leftDownA = Vector2(this->topLeft.x, this->topLeft.y + size.y);
+    rightUpA = Vector2(this->topLeft.x + this->size.x, this->topLeft.y);
+    rightDownA = Vector2(this->topLeft.x + this->size.x, this->topLeft.y + this->size.y);
+
+    //Calculate the sides of rect B
+    leftUpB = b->GetTopLeft();
+    leftDownB = Vector2(b->GetTopLeft().x, b->GetTopLeft().y + b->GetSize().y);
+    rightUpB = Vector2(b->GetTopLeft().x + b->GetSize().x, b->GetTopLeft().y);
+    rightDownB = Vector2(b->GetTopLeft().x + b->GetSize().x, b->GetTopLeft().y + b->GetSize().y);
+
+    //If any of the sides from A are outside of B
+    if (leftUpA.x <= rightDownB.x && leftUpA.y <= rightDownB.y)
+    {
+        return true;
+    }
+
+    if (leftDownA.x <= rightUpB.x && leftDownA.y <= rightUpB.y)
+    {
+        return true;
+    }
+
+    if (rightUpA.x <= leftDownB.x && rightUpA.y <= leftDownB.y)
+    {
+        return true;
+    }
+
+    if (rightDownA.x <= leftUpB.x && rightDownA.y <= leftUpB.y)
+    {
+        return true;
+    }
+
+    //If none of the sides from A are outside B
+    return false;
 }
 
-bool AABB::CheckOverlappingPoint(Vector2)
+bool AABB::CheckOverlappingPoint(Vector2 point)
 {
 	//TODO CheckOverlappingPoint
 	return false;
